b708dda61509673a2c8c84e4c01f728fd317c995
saharghorch <sahar.ghorch@esprit.tn>
2017-05-18 18:41:36 +0200

    Desktop App GestionForums

---
+++ Desktop/src/iservices/iServicepub.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iservices;
+
+import java.util.List;
+
+/**
+ *
+ * @author sahar ghorch
+ */
+public interface iServicepub <T>{
+      void add(T t);
+    void update(T t,int id);
+    void delete(int id);
+    List<T> displayAll();
+   
+}
